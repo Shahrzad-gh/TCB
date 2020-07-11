f9523a97c9d07ae4662a6046330242194b641703
Shahrzad GhahvehRokhi <gh.shahrzad@gmail.com>
2020-07-11 22:16:04 +0430

    article Upload

---
+++ src/views/Typography/Article.js
@@ -0,0 +1,217 @@
+import React, { useState } from "react";
+import axios from "axios";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// core components
+import GridItem from "components/Grid/GridItem.js";
+import Grid from "@material-ui/core/Grid";
+import CustomInput from "components/CustomInput/CustomInput.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardBody from "components/Card/CardBody.js";
+import Button from "components/CustomButtons/Button.js";
+import CardFooter from "components/Card/CardFooter.js";
+import TableList from "../TableList/TableList";
+
+import { Form } from "react-bootstrap";
+
+const styles = {
+  cardCategoryWhite: {
+    "&,& a,& a:hover,& a:focus": {
+      color: "rgba(255,255,255,.62)",
+      margin: "0",
+      fontSize: "14px",
+      marginTop: "0",
+      marginBottom: "0",
+    },
+    "& a,& a:hover,& a:focus": {
+      color: "#FFFFFF",
+    },
+  },
+  cardTitleWhite: {
+    color: "#FFFFFF",
+    marginTop: "0px",
+    minHeight: "auto",
+    fontWeight: "300",
+    fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    marginBottom: "3px",
+    textDecoration: "none",
+    "& small": {
+      color: "#777",
+      fontSize: "65%",
+      fontWeight: "400",
+      lineHeight: "1",
+    },
+  },
+};
+const bgColor = "#0f58d6";
+
+const useStyles = makeStyles(styles);
+
+export default function Article() {
+  const classes = useStyles();
+
+  const [serverState, setServerState] = useState({
+    submitting: false,
+    status: null,
+  });
+
+  const [input, setInput] = useState({
+    name: "",
+    id: "",
+    category: "",
+    description: "",
+    imgFile: "",
+  });
+
+  const handelOnChange = (event) => {
+    console.log("Onchange", event);
+    event.persist();
+    setInput((prev) => ({
+      ...prev,
+      [event.target.id]: event.target.value,
+    }));
+  };
+
+  const handelServerResponse = (ok, msg, form) => {
+    setServerState({
+      submitting: false,
+      status: { ok, msg },
+    });
+    if (ok) {
+      form.reset();
+    }
+  };
+
+  // const handleOnUpload = (event) => {
+  //   console.log("img", event.target.files[0]);
+  //   setInput((prev) => ({
+  //     ...prev,
+  //     [event.target.id]: event.target.value,
+  //   }));
+  // };
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    // {
+    //   name: input.name,
+    //   id: input.id,
+    //   category: input.category,
+    //   description: input.description,
+    //   imgFile: input.imgFile,
+    // };
+    console.log("formData", form);
+    setServerState({ submitting: true });
+    axios({
+      method: "post",
+      url: "https://jsonplaceholder.typicode.com/users",
+      data: new FormData(form),
+    })
+      .then((r) => {
+        handelServerResponse(true, "Thanks for your post", form);
+      })
+      .catch((r) => {
+        handelServerResponse(false, r.response.data.error, form);
+        console.log(handelServerResponse, "something wrong");
+      });
+  };
+
+  return (
+    <GridContainer>
+      <GridItem xs={12} sm={12} md={12}>
+          <CardBody>
+            <Form onSubmit={handelSubmit}>
+              <GridItem xs={12} sm={12} md={6}>
+                <Grid container spacing={3}>
+                  <Grid item xs>
+                    <CustomInput
+                      style={{ padding: 20 }}
+                      labelText="عنوان"
+                      id="titile"
+                      name="title"
+                      type="text"
+                      formControlProps={{
+                        fullWidth: false,
+                        required: true,
+                      }}
+                      inputProps={{
+                        onChange: handelOnChange,
+                        multiline: false,
+                        rows: 1,
+                      }}
+                      value={input.title}
+                    />
+                  </Grid>
+                  <Grid item xs>
+                    <CustomInput
+                      style={{ padding: 20 }}
+                      labelText="دسته بندی مقاله"
+                      formControlProps={{
+                        fullWidth: false,
+                        required: true,
+                      }}
+                      inputProps={{
+                        onChange: handelOnChange,
+                        multiline: true,
+                        rows: 1,
+                      }}
+                      id="category"
+                      type="text"
+                      name="category"
+                      value={input.category}
+                    />
+                  </Grid>
+                </Grid>
+              </GridItem>
+              <GridItem xs={12} sm={12} md={12}>
+                <CustomInput
+                  style={{ padding: 20 }}
+                  labelText="متن مقاله"
+                  formControlProps={{
+                    fullWidth: true,
+                    required: true,
+                  }}
+                  inputProps={{
+                    onChange: handelOnChange,
+                    multiline: true,
+                    rows: 5,
+                  }}
+                  id="description"
+                  type="text"
+                  name="description"
+                  value={input.description}
+                />
+              </GridItem>
+              <GridItem xs={4} sm={4} md={4}>
+                {/*======= Upload Image for product =============*/}
+                <Form.File
+                  onChange={handelOnChange}
+                  style={{ padding: 20 }}
+                  id="imgFile"
+                  label="بار گزاری عکس برای مقاله  "
+                  accept="image/*"
+                  value={input.imgFile}
+                  custom
+                />
+              </GridItem>
+              <Button
+                type="submit"
+                color="warning"
+                disabled={serverState.submitting}
+              >
+                ارسال
+              </Button>
+              {serverState.status && (
+                <p className={!serverState.status.ok ? "errorMsg" : ""}>
+                  {serverState.status.msg}
+                </p>
+              )}
+            </Form>
+          </CardBody>
+          <CardFooter></CardFooter>
+      </GridItem>
+    </GridContainer>
+  );
+}
--- src/views/Typography/Typography.js
+++ src/views/Typography/Typography.js
@@ -12,12 +12,13 @@ import Danger from "components/Typography/Danger.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
+import Article from "./Article";
 
 const styles = {
   typo: {
     paddingLeft: "25%",
     marginBottom: "40px",
-    position: "relative"
+    position: "relative",
   },
   note: {
     fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
@@ -30,14 +31,14 @@ const styles = {
     left: "0",
     marginLeft: "20px",
     position: "absolute",
-    width: "260px"
+    width: "260px",
   },
   cardCategoryWhite: {
     color: "rgba(255,255,255,.62)",
     margin: "0",
     fontSize: "14px",
     marginTop: "0",
-    marginBottom: "0"
+    marginBottom: "0",
   },
   cardTitleWhite: {
     color: "#FFFFFF",
@@ -46,8 +47,8 @@ const styles = {
     fontWeight: "300",
     fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
     marginBottom: "3px",
-    textDecoration: "none"
-  }
+    textDecoration: "none",
+  },
 };
 
 const useStyles = makeStyles(styles);
@@ -57,105 +58,13 @@ export default function TypographyPage() {
   return (
     <Card>
       <CardHeader color="primary">
-        <h4 className={classes.cardTitleWhite}>Material Dashboard Heading</h4>
+        <h4 className={classes.cardTitleWhite}>آپلود مقاله</h4>
         <p className={classes.cardCategoryWhite}>
-          Created using Roboto Font Family
+          {"فونت مورد استفاده ... می باشد"}
         </p>
       </CardHeader>
       <CardBody>
-        <div className={classes.typo}>
-          <div className={classes.note}>Header 1</div>
-          <h1>The Life of Material Dashboard</h1>
-        </div>
-        <div className={classes.typo}>
-          <div className={classes.note}>Header 2</div>
-          <h2>The Life of Material Dashboard</h2>
-        </div>
-        <div className={classes.typo}>
-          <div className={classes.note}>Header 3</div>
-          <h3>The Life of Material Dashboard</h3>
-        </div>
-        <div className={classes.typo}>
-          <div className={classes.note}>Header 4</div>
-          <h4>The Life of Material Dashboard</h4>
-        </div>
-        <div className={classes.typo}>
-          <div className={classes.note}>Header 5</div>
-          <h5>The Life of Material Dashboard</h5>
-        </div>
-        <div className={classes.typo}>
-          <div className={classes.note}>Header 6</div>
-          <h6>The Life of Material Dashboard</h6>
-        </div>
-        <div className={classes.typo}>
-          <div className={classes.note}>Paragraph</div>
-          <p>
-            I will be the leader of a company that ends up being worth billions
-            of dollars, because I got the answers. I understand culture. I am
-            the nucleus. I think that’s a responsibility that I have, to push
-            possibilities, to show people, this is the level that things could
-            be at.
-          </p>
-        </div>
-        <div className={classes.typo}>
-          <div className={classes.note}>Quote</div>
-          <Quote
-            text="I will be the leader of a company that ends up being worth billions of dollars, because I got the answers. I understand culture. I am the nucleus. I think that’s a responsibility that I have, to push possibilities, to show people, this is the level that things could be at."
-            author=" Kanye West, Musician"
-          />
-        </div>
-        <div className={classes.typo}>
-          <div className={classes.note}>Muted Text</div>
-          <Muted>
-            I will be the leader of a company that ends up being worth billions
-            of dollars, because I got the answers...
-          </Muted>
-        </div>
-        <div className={classes.typo}>
-          <div className={classes.note}>Primary Text</div>
-          <Primary>
-            I will be the leader of a company that ends up being worth billions
-            of dollars, because I got the answers...
-          </Primary>
-        </div>
-        <div className={classes.typo}>
-          <div className={classes.note}>Info Text</div>
-          <Info>
-            I will be the leader of a company that ends up being worth billions
-            of dollars, because I got the answers...
-          </Info>
-        </div>
-        <div className={classes.typo}>
-          <div className={classes.note}>Success Text</div>
-          <Success>
-            I will be the leader of a company that ends up being worth billions
-            of dollars, because I got the answers...
-          </Success>
-        </div>
-        <div className={classes.typo}>
-          <div className={classes.note}>Warning Text</div>
-          <Warning>
-            I will be the leader of a company that ends up being worth billions
-            of dollars, because I got the answers...
-          </Warning>
-        </div>
-        <div className={classes.typo}>
-          <div className={classes.note}>Danger Text</div>
-          <Danger>
-            I will be the leader of a company that ends up being worth billions
-            of dollars, because I got the answers...
-          </Danger>
-        </div>
-        <div className={classes.typo}>
-          <div className={classes.note}>Small Tag</div>
-          <h2>
-            Header with small subtitle
-            <br />
-            <small>
-              Use {'"'}Small{'"'} tag for the headers
-            </small>
-          </h2>
-        </div>
+        <Article />
       </CardBody>
     </Card>
   );
